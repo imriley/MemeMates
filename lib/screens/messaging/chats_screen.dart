9027cb9cff3034a0b8edb437f380a25e722757d3
imriley <vishhh0320@gmail.com>
2025-02-10 04:21:41 +0530

    added chats screen

---
+++ lib/screens/messaging/chats_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: const Center(
+        child: Text("Chats Screen"),
+      ),
+    );
+  }
+}
