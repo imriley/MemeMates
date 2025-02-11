f668030316dd7c5b15ce468516f464e3e1fdf5bd
imriley <vishhh0320@gmail.com>
2025-02-12 03:26:35 +0530

    added uid

--- lib/utils/storage/firestore.dart
+++ lib/utils/storage/firestore.dart
@@ -6,12 +6,40 @@ import 'package:firebase_storage/firebase_storage.dart';
 
 Future<void> addUser(mememates.User user) async {
   try {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    final uid = currentUser!.uid;
     final userRef = FirebaseFirestore.instance.collection('users');
-    final uid = user.uid;
-
+    user.uid = uid;
     await userRef.doc(uid).set(user.toMap());
   } catch (e) {
     print('Error adding user: $e');
+  }
+}
+
+Future<List<mememates.User>> fetchAllUsers() async {
+  final currentUser = FirebaseAuth.instance.currentUser;
+  if (currentUser == null) {
+    return [];
+  }
+  final usersCollection = FirebaseFirestore.instance.collection('users');
+  final currentUserUid = currentUser.uid;
+  print("Current User UID: $currentUserUid");
+  final temp = await usersCollection.get();
+  final something = temp.docs[0].data();
+  print(mememates.User.fromMap(something).toString());
+
+  try {
+    final querySnapshot =
+        await usersCollection.where('uid', isNotEqualTo: currentUserUid).get();
+    print("Number of Documents Returned: ${querySnapshot.docs.length}");
+    List<mememates.User> users = querySnapshot.docs.map((doc) {
+      return mememates.User.fromMap(doc.data());
+    }).toList();
+
+    return users;
+  } catch (e) {
+    print('Error fetching users: $e');
+    return [];
   }
 }
 
