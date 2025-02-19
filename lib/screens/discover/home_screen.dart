2b334ce089ed0515a9d80593304daed50b4f488c
imriley <vishhh0320@gmail.com>
2025-02-19 15:20:41 +0530

    added tinder like swipe cards

--- lib/screens/discover/home_screen.dart
+++ lib/screens/discover/home_screen.dart
@@ -1,15 +1,7 @@
-import 'package:audioplayers/audioplayers.dart';
-import 'package:ficonsax/ficonsax.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:icons_plus/icons_plus.dart';
-import 'package:mememates/models/User.dart';
-import 'package:mememates/screens/discover/profile_detail_screen.dart';
-import 'package:mememates/utils/providers/audio_player_provider.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+import 'package:mememates/models/Meme.dart';
 import 'package:mememates/utils/storage/firestore.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -22,93 +14,22 @@ class _HomeScreenState extends State<HomeScreen>
     with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
-  List<User> users = [];
-  int currentCount = 0;
-  String profilePictureUrl = '';
-  late AudioPlayerProvider audioPlayerProvider;
+  int currentIndex = 0;
+  List<Meme> memes = [];
 
   @override
   void initState() {
     super.initState();
-    audioPlayerProvider =
-        Provider.of<AudioPlayerProvider>(context, listen: false);
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      fetchUsersAndPlayAnthem();
+      fetchMemes();
     });
   }
 
-  @override
-  void dispose() {
-    audioPlayerProvider.player.stop();
-    super.dispose();
-  }
-
-  Future<void> fetchUsersAndPlayAnthem() async {
-    final data = await fetchAllUsers();
-    if (mounted) {
-      setState(() {
-        users = data;
-      });
-
-      if (users.isNotEmpty && users[currentCount].profileMusicTitle != null) {
-        await playAnthem(users[currentCount].profileMusicTitle!);
-      }
-    }
-  }
-
-  Future<void> getProfilePictureUrl() async {
-    final user = await getCurrentUser();
+  Future<void> fetchMemes() async {
+    final response = await fetchAllMemes();
     setState(() {
-      profilePictureUrl = user!.profileImageUrl!;
+      memes = response;
     });
-  }
-
-  Future<void> playAnthem(String title) async {
-    if (title.isEmpty) return;
-    final audioUrl = await audioPlayerProvider.fetchYoutubeUrl(title);
-    if (audioUrl == null.toString()) {
-      return;
-    }
-    await audioPlayerProvider.player.setSource(UrlSource(audioUrl));
-    await audioPlayerProvider.player.resume();
-  }
-
-  Future<void> handleDislike() async {
-    await audioPlayerProvider.player.stop();
-    setState(() {
-      users.removeAt(currentCount);
-    });
-    if (users.isNotEmpty) {
-      if (currentCount > 0) {
-        setState(() {
-          currentCount--;
-        });
-      }
-    }
-    if (users.isNotEmpty && users[currentCount].profileMusicTitle != null) {
-      await playAnthem(users[currentCount].profileMusicTitle!);
-    }
-    if (users.isEmpty) {
-      await audioPlayerProvider.player.stop();
-    }
-  }
-
-  Future<void> handleLike() async {
-    await updateLikesAndMatches(users[currentCount]);
-    await audioPlayerProvider.player.stop();
-    setState(() {
-      users.removeAt(currentCount);
-    });
-    if (users.isNotEmpty) {
-      if (currentCount > 0) {
-        setState(() {
-          currentCount--;
-        });
-      }
-    }
-    if (users.isNotEmpty && users[currentCount].profileMusicTitle != null) {
-      await playAnthem(users[currentCount].profileMusicTitle!);
-    }
   }
 
   @override
@@ -116,171 +37,73 @@ class _HomeScreenState extends State<HomeScreen>
     super.build(context);
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.all(
-          16,
-        ),
-        child: Column(
-          children: [
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    CupertinoPageRoute(
-                      builder: (context) => ProfileDetailScreen(
-                        user: users[currentCount],
-                        onDislike: handleDislike,
-                        onLike: handleLike,
-                      ),
-                    ),
-                  );
-                },
-                onVerticalDragUpdate: (details) {
-                  if (details.delta.dy < -5) {
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                        fullscreenDialog: true,
-                        builder: (context) => ProfileDetailScreen(
-                          user: users[currentCount],
-                          onDislike: handleDislike,
-                          onLike: handleLike,
-                        ),
-                      ),
-                    );
-                  }
-                },
-                child: _buildCard(),
-              ),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: memes.isEmpty
+          ? Center(
+              child: Text('No memes found'),
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                _buildActionButton(
-                  icon: AntDesign.close_outline,
-                  color: Colors.orange,
-                  onTap: handleDislike,
-                ),
-                _buildActionButton(
-                  icon: IconsaxBold.heart,
-                  color: const Color(0xFFE94057),
-                  size: 64,
-                  onTap: handleLike,
-                ),
-                _buildActionButton(
-                  icon: Icons.star,
-                  color: Colors.purple,
-                  onTap: () {
-                    // Handle superlike
-                  },
+                Expanded(
+                  child: CardSwiper(
+                    numberOfCardsDisplayed: 1,
+                    allowedSwipeDirection: AllowedSwipeDirection.symmetric(
+                      horizontal: true,
+                      vertical: false,
+                    ),
+                    cardsCount: memes.length,
+                    backCardOffset: Offset(40, 40),
+                    scale: 0.9,
+                    maxAngle: 90,
+                    cardBuilder: (
+                      context,
+                      index,
+                      horizontalThresholdPercentage,
+                      verticalThresholdPercentage,
+                    ) {
+                      final meme = memes[index];
+                      return Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withValues(alpha: 0.05),
+                                  spreadRadius: 2,
+                                  blurRadius: 8,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
+                              gradient: LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  Colors.grey.withValues(alpha: 0.2),
+                                  Colors.grey.withValues(alpha: 0.7),
+                                ],
+                                stops: const [0.7, 1.0],
+                              ),
+                            ),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 8,
+                              ),
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(meme.url),
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
                 ),
               ],
             ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildCard() {
-    if (users.isEmpty) {
-      return Center(
-        child: Text("No users found"),
-      );
-    }
-    final user = users[currentCount];
-    return _buildCarousel(user);
-  }
-
-  Widget _buildCarousel(User user) {
-    return Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: CarouselSlider(
-              options: CarouselOptions(
-                height: double.infinity,
-                enlargeCenterPage: false,
-                viewportFraction: 1.0,
-                initialPage: 0,
-              ),
-              items: [user.profileImageUrl, ...user.moodBoard!.images]
-                  .map((imageUrl) => CachedNetworkImage(
-                        imageUrl: imageUrl!,
-                        placeholder: (context, url) =>
-                            const Center(child: CircularProgressIndicator()),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
-                        fit: BoxFit.cover,
-                        height: double.infinity,
-                        width: double.infinity,
-                      ))
-                  .toList(),
-            ),
-          ),
-        ),
-        Positioned(
-          bottom: 16,
-          left: 16,
-          right: 16,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '${user.name}, ${user.age}',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                user.gender == 'man' ? 'He/Him' : 'She/Her',
-                style: const TextStyle(
-                  color: Colors.white70,
-                  fontSize: 16,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildActionButton({
-    required IconData icon,
-    required Color color,
-    double size = 48,
-    required VoidCallback onTap,
-  }) {
-    return Container(
-      height: size,
-      width: size,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withValues(alpha: 0.2),
-            spreadRadius: 2,
-            blurRadius: 8,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: IconButton(
-        icon: Icon(icon, color: color),
-        onPressed: onTap,
-      ),
     );
   }
 }
